bcc3e7a7caf15ef3a945b9c15ba3aca432626b8f
MateoGonzalezLourido <mlourido1467@gmail.com>
2026-04-04 01:56:25 +0200

    añadido ajustes defecto añadidos ipc faltantes

--- preload.cjs
+++ preload.cjs
@@ -287,6 +287,30 @@ contextBridge.exposeInMainWorld("escaneres_seguridad_app", {
     },
     detectar_url_maliciosa: (text) => {
         return ipcRenderer.invoke("escaneres-seguridad-app-detectar-url-maliciosa", text)
+    },
+    detectar_xss: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-xss", text)
+    },
+    detectar_codigo: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-codigo", text)
+    },
+    detectar_zalgo: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-zalgo", text)
+    },
+    eliminar_zalgo: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-eliminar-zalgo", text)
+    },
+    detectar_comandos_terminal: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-comandos-terminal", text)
+    },
+    detectar_crypto_billeteras: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-crypto-billeteras", text)
+    },
+    detectar_direcciones_ip: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-direcciones-ip", text)
+    },
+    detectar_homoglifos: (text) => {
+        return ipcRenderer.invoke("escaneres-seguridad-app-detectar-homoglifos", text)
     }
 })
 
